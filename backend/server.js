1e3f9d0faff0ccc9be828b487a906fc9d88b5fda
mubarak23523 <mubarak23523@gmail.com>
2026-05-22 19:25:09 +0530

    final responsive and backend fixes

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const userRoutes = require("./routes/userRoutes");
+
 const connectDB = require("./config/db");
+
+const userRoutes = require("./routes/userRoutes");
 const projectRoutes = require("./routes/projectRoutes");
-const app = express();
 const serviceRoutes = require("./routes/serviceRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
+
+const app = express();
+
 // Connect Database
 connectDB();
 
-app.use(cors());
+// CORS FIX
+app.use(
+  cors({
+    origin: "https://increnity.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/services", serviceRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/upload", uploadRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Backend Running...");
 });
